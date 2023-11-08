a912817d0051be5dfb05a35ec6a25d7172f04b6e
Nicola Pirotta <nicolapirotta@gmail.com>
2023-11-08 18:03:07 +0100

    idk what im doing

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -86,6 +86,13 @@ function registerValidSW(swUrl, config) {
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://cra.link/PWA.'
               )
+              if (
+                window.confirm(
+                  'A new version of this app is available. Refresh to update?'
+                )
+              ) {
+                window.location.reload()
+              }
 
               // Execute callback
               if (config && config.onUpdate) {
